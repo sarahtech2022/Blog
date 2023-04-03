bbb487f8dc655455b242c83ed1c7c45a13550315
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-04-03 16:34:07 -0600

    changing template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavBar from './components/Navbar'
-import ListStudents from './components/ListStudents'
+
+import ListBlog from './components/ListBlog'
 
 
 function App() {
 
   return (
     <div className="App">
-      <MyNavBar />
-      <ListStudents />
+   
+      <ListBlog />
 
     </div>
   )
--- client/src/components/ListBlog.jsx
+++ client/src/components/ListBlog.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import * as ioicons from 'react-icons/io5'
 import MyForm from './Form';
-import Student from './Student';
+import BlogCard from './BlogCard';
 
-const ListStudents = () => {
+const ListBlog = () => {
 
     // this is my original state with an array of students 
     const [students, setStudents] = useState([]);
@@ -62,10 +62,10 @@ const ListStudents = () => {
     return (
         <div className="mybody">
         <div className="list-students">
-            <h2>Techtonica Participants </h2>
+            <h2>Blog Posts </h2>
             <ul>
                 {students.map((student) => {
-                    return <li key={student.id}> <Student student={student} toDelete={onDelete} toUpdate={onUpdate} /></li>
+                    return <li key={student.id}> <BlogCard student={student} toDelete={onDelete} toUpdate={onUpdate} /></li>
                 })}
             </ul>
         </div>
@@ -75,4 +75,4 @@ const ListStudents = () => {
 }
 
 
-export default ListStudents
+export default ListBlog
