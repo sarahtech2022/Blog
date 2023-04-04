896d4e27eb32e18ed158d0d5b49790959be04b67
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-04-03 23:26:57 -0600

    saving changes so far for blogpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import BlogPage from './components/BlogPage';
 
 import ListBlog from './components/ListBlog'
+
+
 
 
 function App() {
 
   return (
     <div className="App">
-   
+      <Routes>
+        <Route path="blogpage" element={<BlogPage />} />
+       
+      </Routes>
+
       <ListBlog />
+
 
     </div>
   )
